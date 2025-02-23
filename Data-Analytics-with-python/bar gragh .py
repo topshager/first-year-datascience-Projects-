436eb391ef6b2139b25f62c5062cd2efb2396ab1
topshager <rem28777@gmail.com>
2025-02-23 22:06:34 +0200

    Delete bar gragh .py

--- Data-Analytics-with-python/bar gragh .py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.bar(['18 – 25','25 – 35','35 – 45','over 45 '],[42,72,36,54],width=0.3,color='blue')
-plt.xlabel('Age groupe')
-plt.ylabel('Number of students')
-plt.title('University student Ages')
-plt.show()
-
-
